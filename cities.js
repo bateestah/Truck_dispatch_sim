32c1b7c190ed01b960bddbf1f9a77b494bb1fea2
Alex <bateestah@gmail.com>
2025-08-23 16:26:09 -0400

    Add files via upload

---
+++ cities.js
@@ -0,0 +1,244 @@
+export const CityGroups = [
+  {state:'Alabama', items:[
+    {name:'Birmingham, AL',lat:33.5186,lng:-86.8104},
+    {name:'Montgomery, AL',lat:32.3668,lng:-86.3000},
+    {name:'Huntsville, AL',lat:34.7304,lng:-86.5861}
+  ]},
+  {state:'Arizona', items:[
+    {name:'Phoenix, AZ',lat:33.4484,lng:-112.0740},
+    {name:'Tucson, AZ',lat:32.2226,lng:-110.9747},
+    {name:'Mesa, AZ',lat:33.4152,lng:-111.8315}
+  ]},
+  {state:'Arkansas', items:[
+    {name:'Little Rock, AR',lat:34.7465,lng:-92.2896},
+    {name:'Fort Smith, AR',lat:35.3859,lng:-94.3985},
+    {name:'Fayetteville, AR',lat:36.0626,lng:-94.1574}
+  ]},
+  {state:'California', items:[
+    {name:'Los Angeles, CA',lat:34.0522,lng:-118.2437},
+    {name:'San Diego, CA',lat:32.7157,lng:-117.1611},
+    {name:'San Jose, CA',lat:37.3382,lng:-121.8863}
+  ]},
+  {state:'Colorado', items:[
+    {name:'Denver, CO',lat:39.7392,lng:-104.9903},
+    {name:'Colorado Springs, CO',lat:38.8339,lng:-104.8214},
+    {name:'Aurora, CO',lat:39.7294,lng:-104.8319}
+  ]},
+  {state:'Connecticut', items:[
+    {name:'Bridgeport, CT',lat:41.1792,lng:-73.1894},
+    {name:'New Haven, CT',lat:41.3083,lng:-72.9279},
+    {name:'Stamford, CT',lat:41.0534,lng:-73.5387}
+  ]},
+  {state:'Delaware', items:[
+    {name:'Wilmington, DE',lat:39.7391,lng:-75.5398},
+    {name:'Dover, DE',lat:39.1582,lng:-75.5244},
+    {name:'Newark, DE',lat:39.6837,lng:-75.7497}
+  ]},
+  {state:'Florida', items:[
+    {name:'Jacksonville, FL',lat:30.3322,lng:-81.6557},
+    {name:'Miami, FL',lat:25.7617,lng:-80.1918},
+    {name:'Tampa, FL',lat:27.9506,lng:-82.4572}
+  ]},
+  {state:'Georgia', items:[
+    {name:'Atlanta, GA',lat:33.7490,lng:-84.3880},
+    {name:'Columbus, GA',lat:32.46098,lng:-84.9877},
+    {name:'Augusta, GA',lat:33.4735,lng:-82.0105}
+  ]},
+  {state:'Idaho', items:[
+    {name:'Boise, ID',lat:43.6150,lng:-116.2023},
+    {name:'Nampa, ID',lat:43.5407,lng:-116.5635},
+    {name:'Meridian, ID',lat:43.6121,lng:-116.3915}
+  ]},
+  {state:'Illinois', items:[
+    {name:'Chicago, IL',lat:41.8781,lng:-87.6298},
+    {name:'Aurora, IL',lat:41.7606,lng:-88.3201},
+    {name:'Joliet, IL',lat:41.5250,lng:-88.0817}
+  ]},
+  {state:'Indiana', items:[
+    {name:'Indianapolis, IN',lat:39.7684,lng:-86.1581},
+    {name:'Fort Wayne, IN',lat:41.0793,lng:-85.1394},
+    {name:'Evansville, IN',lat:37.9716,lng:-87.5711}
+  ]},
+  {state:'Iowa', items:[
+    {name:'Des Moines, IA',lat:41.5868,lng:-93.6250},
+    {name:'Cedar Rapids, IA',lat:41.9779,lng:-91.6656},
+    {name:'Davenport, IA',lat:41.5236,lng:-90.5776}
+  ]},
+  {state:'Kansas', items:[
+    {name:'Wichita, KS',lat:37.6872,lng:-97.3301},
+    {name:'Overland Park, KS',lat:38.9822,lng:-94.6708},
+    {name:'Kansas City, KS',lat:39.1142,lng:-94.6275}
+  ]},
+  {state:'Kentucky', items:[
+    {name:'Louisville, KY',lat:38.2527,lng:-85.7585},
+    {name:'Lexington, KY',lat:38.0406,lng:-84.5037},
+    {name:'Bowling Green, KY',lat:36.9685,lng:-86.4808}
+  ]},
+  {state:'Louisiana', items:[
+    {name:'New Orleans, LA',lat:29.9511,lng:-90.0715},
+    {name:'Baton Rouge, LA',lat:30.4515,lng:-91.1871},
+    {name:'Shreveport, LA',lat:32.5252,lng:-93.7502}
+  ]},
+  {state:'Maine', items:[
+    {name:'Portland, ME',lat:43.6591,lng:-70.2568},
+    {name:'Lewiston, ME',lat:44.1004,lng:-70.2148},
+    {name:'Bangor, ME',lat:44.8012,lng:-68.7778}
+  ]},
+  {state:'Maryland', items:[
+    {name:'Baltimore, MD',lat:39.2904,lng:-76.6122},
+    {name:'Frederick, MD',lat:39.4143,lng:-77.4105},
+    {name:'Rockville, MD',lat:39.0834,lng:-77.1528}
+  ]},
+  {state:'Massachusetts', items:[
+    {name:'Boston, MA',lat:42.3601,lng:-71.0589},
+    {name:'Worcester, MA',lat:42.2626,lng:-71.8023},
+    {name:'Springfield, MA',lat:42.1015,lng:-72.5898}
+  ]},
+  {state:'Michigan', items:[
+    {name:'Detroit, MI',lat:42.3314,lng:-83.0458},
+    {name:'Grand Rapids, MI',lat:42.9634,lng:-85.6681},
+    {name:'Warren, MI',lat:42.5145,lng:-83.0147}
+  ]},
+  {state:'Minnesota', items:[
+    {name:'Minneapolis, MN',lat:44.9778,lng:-93.2650},
+    {name:'Saint Paul, MN',lat:44.9537,lng:-93.0900},
+    {name:'Rochester, MN',lat:44.0121,lng:-92.4802}
+  ]},
+  {state:'Mississippi', items:[
+    {name:'Jackson, MS',lat:32.2988,lng:-90.1848},
+    {name:'Gulfport, MS',lat:30.3674,lng:-89.0928},
+    {name:'Southaven, MS',lat:34.9890,lng:-90.0126}
+  ]},
+  {state:'Missouri', items:[
+    {name:'Kansas City, MO',lat:39.0997,lng:-94.5786},
+    {name:'St. Louis, MO',lat:38.6270,lng:-90.1994},
+    {name:'Springfield, MO',lat:37.2089,lng:-93.2923}
+  ]},
+  {state:'Montana', items:[
+    {name:'Billings, MT',lat:45.7833,lng:-108.5007},
+    {name:'Missoula, MT',lat:46.8721,lng:-113.9940},
+    {name:'Great Falls, MT',lat:47.5053,lng:-111.3008}
+  ]},
+  {state:'Nebraska', items:[
+    {name:'Omaha, NE',lat:41.2565,lng:-95.9345},
+    {name:'Lincoln, NE',lat:40.8136,lng:-96.7026},
+    {name:'Bellevue, NE',lat:41.1544,lng:-95.9146}
+  ]},
+  {state:'Nevada', items:[
+    {name:'Las Vegas, NV',lat:36.1699,lng:-115.1398},
+    {name:'Henderson, NV',lat:36.0395,lng:-114.9817},
+    {name:'Reno, NV',lat:39.5296,lng:-119.8138}
+  ]},
+  {state:'New Hampshire', items:[
+    {name:'Manchester, NH',lat:42.9956,lng:-71.4548},
+    {name:'Nashua, NH',lat:42.7654,lng:-71.4676},
+    {name:'Concord, NH',lat:43.2081,lng:-71.5376}
+  ]},
+  {state:'New Jersey', items:[
+    {name:'Newark, NJ',lat:40.7357,lng:-74.1724},
+    {name:'Jersey City, NJ',lat:40.7178,lng:-74.0431},
+    {name:'Paterson, NJ',lat:40.9168,lng:-74.1718}
+  ]},
+  {state:'New Mexico', items:[
+    {name:'Albuquerque, NM',lat:35.0844,lng:-106.6504},
+    {name:'Las Cruces, NM',lat:32.3199,lng:-106.7637},
+    {name:'Rio Rancho, NM',lat:35.2328,lng:-106.6630}
+  ]},
+  {state:'New York', items:[
+    {name:'New York, NY',lat:40.7128,lng:-74.0060},
+    {name:'Buffalo, NY',lat:42.8864,lng:-78.8784},
+    {name:'Rochester, NY',lat:43.1566,lng:-77.6088}
+  ]},
+  {state:'North Carolina', items:[
+    {name:'Charlotte, NC',lat:35.2271,lng:-80.8431},
+    {name:'Raleigh, NC',lat:35.7796,lng:-78.6382},
+    {name:'Greensboro, NC',lat:36.0726,lng:-79.7920}
+  ]},
+  {state:'North Dakota', items:[
+    {name:'Fargo, ND',lat:46.8772,lng:-96.7898},
+    {name:'Bismarck, ND',lat:46.8083,lng:-100.7837},
+    {name:'Grand Forks, ND',lat:47.9253,lng:-97.0329}
+  ]},
+  {state:'Ohio', items:[
+    {name:'Columbus, OH',lat:39.9612,lng:-82.9988},
+    {name:'Cleveland, OH',lat:41.4993,lng:-81.6944},
+    {name:'Cincinnati, OH',lat:39.1031,lng:-84.5120}
+  ]},
+  {state:'Oklahoma', items:[
+    {name:'Oklahoma City, OK',lat:35.4676,lng:-97.5164},
+    {name:'Tulsa, OK',lat:36.1540,lng:-95.9928},
+    {name:'Norman, OK',lat:35.2226,lng:-97.4395}
+  ]},
+  {state:'Oregon', items:[
+    {name:'Portland, OR',lat:45.5152,lng:-122.6784},
+    {name:'Salem, OR',lat:44.9429,lng:-123.0351},
+    {name:'Eugene, OR',lat:44.0521,lng:-123.0868}
+  ]},
+  {state:'Pennsylvania', items:[
+    {name:'Philadelphia, PA',lat:39.9526,lng:-75.1652},
+    {name:'Pittsburgh, PA',lat:40.4406,lng:-79.9959},
+    {name:'Allentown, PA',lat:40.6023,lng:-75.4714}
+  ]},
+  {state:'Rhode Island', items:[
+    {name:'Providence, RI',lat:41.8240,lng:-71.4128},
+    {name:'Warwick, RI',lat:41.7001,lng:-71.4162},
+    {name:'Cranston, RI',lat:41.7798,lng:-71.4373}
+  ]},
+  {state:'South Carolina', items:[
+    {name:'Columbia, SC',lat:34.0007,lng:-81.0348},
+    {name:'Charleston, SC',lat:32.7765,lng:-79.9311},
+    {name:'North Charleston, SC',lat:32.8546,lng:-79.9748}
+  ]},
+  {state:'South Dakota', items:[
+    {name:'Sioux Falls, SD',lat:43.5446,lng:-96.7311},
+    {name:'Rapid City, SD',lat:44.0805,lng:-103.2310},
+    {name:'Aberdeen, SD',lat:45.4647,lng:-98.4865}
+  ]},
+  {state:'Tennessee', items:[
+    {name:'Memphis, TN',lat:35.1495,lng:-90.0490},
+    {name:'Nashville, TN',lat:36.1627,lng:-86.7816},
+    {name:'Knoxville, TN',lat:35.9606,lng:-83.9207}
+  ]},
+  {state:'Texas', items:[
+    {name:'Houston, TX',lat:29.7604,lng:-95.3698},
+    {name:'San Antonio, TX',lat:29.4241,lng:-98.4936},
+    {name:'Dallas, TX',lat:32.7767,lng:-96.7970}
+  ]},
+  {state:'Utah', items:[
+    {name:'Salt Lake City, UT',lat:40.7608,lng:-111.8910},
+    {name:'West Valley City, UT',lat:40.6916,lng:-112.0010},
+    {name:'Provo, UT',lat:40.2338,lng:-111.6585}
+  ]},
+  {state:'Vermont', items:[
+    {name:'Burlington, VT',lat:44.4759,lng:-73.2121},
+    {name:'South Burlington, VT',lat:44.4670,lng:-73.1710},
+    {name:'Rutland, VT',lat:43.6106,lng:-72.9726}
+  ]},
+  {state:'Virginia', items:[
+    {name:'Virginia Beach, VA',lat:36.8529,lng:-75.9780},
+    {name:'Norfolk, VA',lat:36.8508,lng:-76.2859},
+    {name:'Chesapeake, VA',lat:36.7682,lng:-76.2875}
+  ]},
+  {state:'Washington', items:[
+    {name:'Seattle, WA',lat:47.6062,lng:-122.3321},
+    {name:'Spokane, WA',lat:47.6588,lng:-117.4260},
+    {name:'Tacoma, WA',lat:47.2529,lng:-122.4443}
+  ]},
+  {state:'West Virginia', items:[
+    {name:'Charleston, WV',lat:38.3498,lng:-81.6326},
+    {name:'Huntington, WV',lat:38.4192,lng:-82.4452},
+    {name:'Morgantown, WV',lat:39.6295,lng:-79.9559}
+  ]},
+  {state:'Wisconsin', items:[
+    {name:'Milwaukee, WI',lat:43.0389,lng:-87.9065},
+    {name:'Madison, WI',lat:43.0731,lng:-89.4012},
+    {name:'Green Bay, WI',lat:44.5133,lng:-88.0133}
+  ]},
+  {state:'Wyoming', items:[
+    {name:'Cheyenne, WY',lat:41.1400,lng:-104.8202},
+    {name:'Casper, WY',lat:42.8501,lng:-106.3252},
+    {name:'Laramie, WY',lat:41.3114,lng:-105.5911}
+  ]}
+];
+export const Cities = CityGroups.flatMap(g => g.items);
+export function cityByName(name){ const c=Cities.find(x=>x.name===name); if(!c) throw new Error('City not found: '+name); return c; }
