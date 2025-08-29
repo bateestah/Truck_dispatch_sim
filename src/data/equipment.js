fbd8ad9f3265e7431a98baeefb1cf2ac3b0f6acc
Alex <bateestah@gmail.com>
2025-08-28 20:55:32 -0400

    Add equipment data for market

---
+++ src/data/equipment.js
@@ -0,0 +1,21 @@
+export const TRUCK_MODELS = [
+  { make: 'Freightliner', model: 'Cascadia', variants: ['Sleeper', 'Day Cab'] },
+  { make: 'Peterbilt', model: '579', variants: ['Sleeper', 'Day Cab'] },
+  { make: 'Peterbilt', model: '589', variants: ['Sleeper'] },
+  { make: 'Kenworth', model: 'T680', variants: ['Sleeper', 'Day Cab'] },
+  { make: 'Kenworth', model: 'W900', variants: ['Sleeper'] },
+  { make: 'Western Star', model: '57X', variants: ['Sleeper'] },
+  { make: 'Volvo', model: 'VNL 860', variants: ['Sleeper'] },
+  { make: 'Volvo', model: 'VNL 660', variants: ['Sleeper'] },
+  { make: 'Volvo', model: 'VNL 300', variants: ['Day Cab'] },
+  { make: 'Mack', model: 'Anthem', variants: ['Sleeper', 'Day Cab'] },
+  { make: 'International', model: 'LT', variants: ['Sleeper', 'Day Cab'] },
+  { make: 'International', model: 'LoneStar', variants: ['Sleeper'] }
+];
+
+export const TRAILER_MODELS = [
+  { brand: 'Hyundai', types: ['Dry Van', 'Reefer'] },
+  { brand: 'Wabash', types: ['Dry Van', 'Reefer', 'Tanker'] },
+  { brand: 'Great Dane', types: ['Dry Van', 'Reefer', 'Flatbed'] },
+  { brand: 'Fontaine', types: ['Flatbed'] }
+];
